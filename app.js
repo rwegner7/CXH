f6e16b040bb2dd558b0b4dbff76a24418f9a2d97
rwegner7 <wegner.r777@yahoo.com>
2017-11-17 10:20:44 -0700

    updating

---
+++ app.js
@@ -0,0 +1,45 @@
+var nforce = require('nforce');
+var express = require('express');
+var port = process.env.PORT || 3000;
+
+var org = nforce.createConnection({
+  clientId: '3MVG9d3kx8wbPieE1owJgtExii8HVUB8wNIpym1tet6IuuMjgrejihFQOohd4weG6X.P2ZqMYSciZP0EL9HoH',
+  clientSecret: '7324949617154192227',
+  redirectUri: 'https://cxh-lightningout.herokuapp.com/oauth/_callback',
+  apiVersion: 'v34.0',  // optional, defaults to current salesforce API version
+  environment: 'sandbox',  // optional, salesforce 'sandbox' or 'production', production default
+  mode: 'multi' // optional, 'single' or 'multi' user mode, multi default
+});
+
+var app = express();
+
+// Require Routes js
+var routesHome = require('./routes/home');
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+app.use('/home', routesHome);
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+  res.redirect(org.getAuthUri());
+});
+
+app.get('/oauth/_callback', function(req, res) {
+  org.authenticate({code: req.query.code}, function(err, resp){
+    if(!err) {
+      console.log('Access Token: ' + resp.access_token);
+      app.locals.oauthtoken = resp.access_token;
+      app.locals.lightningEndPointURI = "https://cpscxh2--rwegner.lightning.force.com";
+      res.redirect('/home');
+    } else {
+      console.log('Error: ' + err.message);
+    }
+  });
+});
+
+// Served Localhost
+console.log('Served: http://localhost:' + port);
+app.listen(port);
